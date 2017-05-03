9baa643488c0b4eba0e2b28542c94e8f2938ad01
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2017-05-04 01:55:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# purescript-smolder-idom
+
+Smolder renderer for [Incremental DOM](https://github.com/google/incremental-dom).
---
+++ src/Text/Smolder/Renderer/IncrementalDom.purs
@@ -0,0 +1,45 @@
+module Text.Smolder.Renderer.IncrementalDom (render) where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import DOM.Event.Event (Event)
+import DOM.Event.EventTarget (eventListener)
+import Data.Array (singleton)
+import Data.CatList (CatList)
+import Data.Foldable (foldMap)
+import Data.Foreign (Foreign, toForeign)
+import Data.Maybe (Maybe(..))
+import Data.StrMap (StrMap, toUnfoldable)
+import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..))
+import Text.Smolder.Markup (EventHandler(..), Markup)
+import Text.Smolder.Renderer.Util (Node(..), renderMarkup)
+import Web.IncrementalDOM (IDOM, elementClose, elementOpen, text)
+
+renderAttributes :: StrMap String -> Array (Tuple String Foreign)
+renderAttributes = map toForeign >>> toUnfoldable
+
+renderListener :: forall e. EventHandler (Event -> Eff e Unit) -> Tuple String Foreign
+renderListener (EventHandler eventName callback) =
+  Tuple ("on" <> eventName) (toForeign $ eventListener callback)
+
+renderListeners ::
+  forall e.
+  CatList (EventHandler (Event -> Eff e Unit)) ->
+  Array (Tuple String Foreign)
+renderListeners = foldMap (renderListener >>> singleton)
+
+renderNode :: forall e. Node (Event -> Eff e Unit) -> Eff (idom :: IDOM | e) Unit
+renderNode (Text t) = text t *> pure unit
+renderNode (Element name props listeners children) = do
+  _ <-
+    elementOpen name Nothing []
+    (renderAttributes props <> renderListeners listeners)
+  _ <- traverse renderNode children
+  _ <- elementClose name
+  pure unit
+
+render :: forall e. Markup (Event -> Eff e Unit) -> Eff (idom :: IDOM | e) Unit
+render markup = do
+  _ <- traverse renderNode $ renderMarkup markup
+  pure unit
